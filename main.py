ab3c5ab1010c6e2e3d321355b4df25619e49a771
Luka <luka.akhalkatsi@makingscience.com>
2026-02-20 18:52:41 +0400

    feat: add main

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from google.cloud import pubsub_v1
+from bigquery_checker.check import BigQueryClient
+import json
+from utils.settings import settings
+from utils.logger import logger
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main(request):
+    launcher = BigQueryClient()
+    missing_dates = launcher.get_missing_dates_from_bigquery()
+    if not missing_dates:
+        logger.info("Missing dates do not exist")
+        return {"info": "missing dates do not exist"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    topic_id = settings.TOPIC_ID
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(settings.PROJECT_ID, topic_id)
+
+
+    logger.info("Missing dates", missing_dates)
+
+    futures = []
+
+    for date in missing_dates:
+        message = {
+            "start_date": date,
+            "end_date": date
+        }
+
+        logger.info("Publishing:", message)
+
+        message_bytes = json.dumps(message).encode("utf-8   ")
+        future = publisher.publish(topic_path, message_bytes)
+        futures.append(future)
+
+    for future in futures:
+        logger.info(future.result())
+
+    return {"success"}, 200
